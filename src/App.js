2535c5e6e116ed240cb7f2c11f692c7f02d985bb
Charly_Omar <cona_17@outlook.com>
2024-10-16 18:11:12 -0600

    Se han añadido los archivos para el inicio de sesión

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import logo from './imgs/img.jpg'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>Bienvenido</p>
       </header>
+      <body className="App-body">
+        <div className="Container Container-img rounded m-4">
+          <img src={logo} alt="Imagen de ejemplo" className="img-fluid rounded"/>
+        </div>
+        <div className="Container Container-login rounded m-4">
+          <form className="App-body-form">
+            <h1>Iniciar sesión</h1>
+            <h3>¡Que bueno verte por aquí!</h3>
+            <input className="user form-control" type="text" hint="Ingresa tu usuario"></input>
+            <input className="password form-control" type="password" hint="Ingresa tu contraseña"></input>
+            <button type="submit" className="btn btn-primary">Iniciar sesión</button>
+            <a href="http://localhost:3000/">Olvidé mi contraseña :c</a>
+          </form>
+        </div>
+      </body>
+      <footer className="App-footer">
+        <ul>
+          <li><a href="">Acerca de nosotros</a></li>
+          <li><a href="">Contactanos</a></li>
+          <li><a href="">Términos y privacidad</a></li>
+        </ul>
+      </footer>
     </div>
   );
 }
